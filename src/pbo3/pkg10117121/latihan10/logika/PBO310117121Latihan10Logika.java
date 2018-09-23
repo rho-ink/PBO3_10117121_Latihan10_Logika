952ac230469fd67b8429e4616354984d1a8d6c84
CHEEKIBREEKIIVDAMKE <CHEEKIBREEKIIVDAMKE@DESKTOP-S13CU08>
2018-09-23 18:57:02 +0700

    rho-ink

---
+++ src/pbo3/pkg10117121/latihan10/logika/PBO310117121Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117121.latihan10.logika;
+
+/**
+ *
+ * @author CHEEKIBREEKIIVDAMKE
+ *NAMA                 : M Rifqi S M
+ *KELAS                : PBO-3 
+ *NIM                  : 10117121
+ *Program Deskripsi    : Program ini berisi program menampilkan fungsi logika
+ */
+public class PBO310117121Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
